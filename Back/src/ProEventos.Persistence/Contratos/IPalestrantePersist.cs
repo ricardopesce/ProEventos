36b22eb078e76aa5be4ecbe7110e3b3302279433
Ricardo Pesce <rspesce@gmail.com>
2022-04-22 21:24:25 -0300

    Seção 6: .NET Core - Organizando Camadas

---
+++ Back/src/ProEventos.Persistence/Contratos/IPalestrantePersist.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IPalestrantePersist
+    {
+         Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos = false);
+         Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos = false);
+         Task<Palestrante> GetPalestranteByIdAsync(int palestranteId, bool includeEventos = false);
+
+    }
+}
